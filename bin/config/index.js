4ab76af0fc719021e02f3c1f3a9a1fc611f859ff
echo hu <huwanfei125@163.com>
2021-04-07 10:36:36 +0800

    feat✨ 新增移动端微信公众号模版

--- bin/config/index.js
+++ bin/config/index.js
@@ -1,10 +1,10 @@
 /*
  * @Author: huwanfei
  * @Date: 2021-02-24 15:06:19
- * @LastEditTime: 2021-02-24 15:58:09
+ * @LastEditTime: 2021-04-07 10:36:01
  * @LastEditors: huwanfei
  * @Description:  配置文件
- * @FilePath: /quick-cli/bin/config/index.js
+ * @FilePath: /rapid-cli/bin/config/index.js
  */
 module.exports = {
   npmUrl: 'https://registry.npmjs.org/rapid-datav-cli',
@@ -35,6 +35,14 @@ module.exports = {
           url: 'http://git.dev.wochanye.com/development-team/uniapp.git',
           gitName: 'uniapp',
           val: 'uniapp模版'
+        }
+      },
+      {
+        name: 'vue-weapp',
+        value: {
+          url: 'https://github.com/candy415/weapp-template.git',
+          gitName: 'weapp',
+          val: '微信公众号模版'
         }
       }
     ]
